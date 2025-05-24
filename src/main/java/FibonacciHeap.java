ecef3c846a16fbc3a7b0d828cb13133f40a311a5
Артём <a.yunusov2006@gmail.com>
2025-05-24 13:09:17 +0300

    first commit

---
+++ src/main/java/FibonacciHeap.java
@@ -0,0 +1,110 @@
+import java.util.*;
+
+public class FibonacciHeap {
+    private int size;
+    private Node min;
+
+    public FibonacciHeap() {
+        this.size = 0;
+        this.min = null;
+    }
+
+    public boolean isEmpty() {
+        return min == null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public Integer getMin() {
+        return min != null ? min.getKey() : null;
+    }
+
+    public void clear() {
+        min = null;
+        size = 0;
+    }
+
+    public Node insert(Integer key) {
+        Node newNode = new Node(key);
+        mergeLists(min, newNode);
+        size++;
+        return newNode;
+    }
+
+    public void mergeHeaps(FibonacciHeap heap2) {
+        if(heap2.isEmpty() || heap2 == null) return;
+        min = mergeLists(min, heap2.min);
+        size += heap2.size;
+        heap2.clear();
+    }
+
+    public void consolidate() {
+        int arraySize = ((int) Math.floor(Math.log(size) / Math.log(2))) + 1;
+        List<Node> degreeList = new ArrayList<>(Collections.nCopies(arraySize, null));
+
+        List<Node> rootList = new ArrayList<>();
+        Node currentRoot = min;
+        if (currentRoot != null) {
+            do {
+                rootList.add(currentRoot);
+                currentRoot = currentRoot.getRight();
+            } while (currentRoot != min);
+        }
+
+        for(Node currentTree:rootList) {
+            int degree = currentTree.getDegree();
+
+            while(degreeList.get(degree) != null) {
+                Node existingTree = degreeList.get(degree);
+
+                if(currentTree.getKey() > existingTree.getKey()) {
+                    Node temp = currentTree;
+                    currentTree = existingTree;
+                    existingTree = temp;
+                }
+
+                link(existingTree, currentTree);
+                degreeList.set(degree, null);
+                degree++;
+            }
+            degreeList.set(degree, currentTree);
+        }
+
+        min = null;
+        for (Node node : degreeList) {
+            if (node != null) {
+                min = mergeLists(min, node);
+            }
+        }
+    }
+
+    private void link(Node child, Node parent) {
+        child.getLeft().setRight(child.getRight());
+        child.getRight().setLeft(child.getLeft());
+        child.setLeft(child);
+        child.setRight(child);
+        child.setParent(parent);
+
+        parent.setChild(mergeLists(parent.getChild(), child));
+        parent.setDegree(parent.getDegree() + 1);
+        child.setMark(false);
+    }
+
+    private Node mergeLists(Node a, Node b) {
+        if (a == null) return b;
+        if (b == null) return a;
+
+        Node aRight = a.getRight();
+        Node bRight = b.getRight();
+
+        a.setRight(bRight);
+        bRight.setLeft(a);
+        b.setRight(aRight);
+        aRight.setLeft(b);
+
+        return a.getKey() < b.getKey() ? a : b;
+    }
+
+}
---
+++ src/main/java/Node.java
@@ -0,0 +1,81 @@
+public class Node {
+    private Integer key;
+    private int degree;
+    private boolean mark;
+    private Node parent;
+    private Node child;
+    private Node left;
+    private Node right;
+
+    public Node (Integer key) {
+        this.key = key;
+        this.degree = 0;
+        this.mark = false;
+        this.parent = null;
+        this.child = null;
+        this.left = this;
+        this.right = this;
+
+    }
+
+    public Integer getKey() {
+        return key;
+    }
+
+    public void setKey(Integer key) {
+        this.key = key;
+    }
+
+    public int getDegree() {
+        return degree;
+    }
+
+    public void setDegree(int degree) {
+        this.degree = degree;
+    }
+
+    public boolean isMark() {
+        return mark;
+    }
+
+    public void setMark(boolean mark) {
+        this.mark = mark;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public Node getChild() {
+        return child;
+    }
+
+    public void setChild(Node child) {
+        this.child = child;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return key.toString();
+    }
+}
